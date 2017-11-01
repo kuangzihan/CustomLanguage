2656c7a6759844311ec577e025d1a6dd10354e98
Niko <836747765@qq.com>
2017-11-01 09:08:57 +0800

    更新
    更新

--- 多语言切换/多语言切换/CustomLanguage.swift
+++ 多语言切换/多语言切换/CustomLanguage.swift
@@ -16,7 +16,9 @@ class CustomLanguage: NSObject {
     var lan = ""
     
     func showText(key: String) -> String {
+        // 查找对应国家语言代码的路径
         guard let path = Bundle.main.path(forResource: lan, ofType: "lproj") else { return "" }
+        // 通过路径在CustomLacation语言文件中查找对应key的字符串
         guard let bundle = Bundle(path: path)?.localizedString(forKey: key, value: nil, table: "CustomLacation") else { return "" }
         return bundle
     }
